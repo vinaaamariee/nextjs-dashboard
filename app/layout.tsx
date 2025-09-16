54a43946c7446df3202a871b574e43fef102f8ee
vina marie <cabreravinamarie@gmail.com>
2025-09-16 18:49:44 +0800

    more changes
    added a new files and folder.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
 import '@/app/ui/global.css';
 import { inter }  from '@/app/ui/fonts';
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import Link from 'next/link'
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body  className={`${inter.className} antialiased`}>{children}</body>
+    <html>
+      <body>
+        <nav>
+          {/* Prefetched when the link is hovered or enters the viewport */}
+          <Link href="/blog">Blog</Link>
+          {/* No prefetching */}
+          <a href="/contact">Contact</a>
+        </nav>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
