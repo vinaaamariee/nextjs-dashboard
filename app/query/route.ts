283d883aacd46ab376f507878f25f43098c4ba1d
vina marie <cabreravinamarie@gmail.com>
2025-10-14 19:02:45 +0800

    none

--- app/query/route.ts
+++ app/query/route.ts
@@ -4,10 +4,10 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listInvoices() {
 	const data = await sql`
-    SELECT invoices.amount, customers.name
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
+   SELECT invoices.amount, customers.name
+FROM invoices
+JOIN customers ON invoices.customer_id = customers.id
+WHERE invoices.amount = 666;
   `;
 
 	return data;
