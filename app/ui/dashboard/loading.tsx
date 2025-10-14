43ebacadab4a0a42b18389259ff23b1c8a60adef
vina marie <cabreravinamarie@gmail.com>
2025-10-14 09:50:29 +0800

    updated loading.tsx

--- app/ui/dashboard/loading.tsx
+++ app/ui/dashboard/loading.tsx
@@ -1,4 +1,5 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
 export default function Loading() {
   // Add fallback UI that will be shown while the route is loading.
-  return <LoadingSkeleton />
+  return <DashboardSkeleton />
 }
