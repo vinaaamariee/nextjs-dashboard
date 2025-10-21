c8ec4cc910127171ec98e6abe4722ef8341e5e3b
vina marie <cabreravinamarie@gmail.com>
2025-10-21 19:21:42 +0800

    updates

--- app/dashboard/vinacabrera/page.tsx
+++ app/dashboard/vinacabrera/page.tsx
@@ -1,20 +1,14 @@
-
+"use client";
 export default function Page() {
-    return(
-        <div style={{height:'100vh'}}>
-        <iframe
-            src="https://github.com/vinaaamariee/my_portfolio"
-            style={{
-                border: 'none',
-                width: '100%',
-                height: '100%',
-            }}
-            title="My Portfolio"
-            />
-
-        
-            
-       </div>
-        
-    )
+  return (
+    <iframe
+      src="https://vinaaamariee.github.io/my_portfolio/"
+      style={{
+        border: 'none',
+        width: '100%',
+        height: '100vh',
+      }}
+      title="My Portfolio"
+    />
+  );
 }
