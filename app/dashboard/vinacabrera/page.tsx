a81d5bbd26a26db4d1d4c344f43ed40907c17867
vina marie <cabreravinamarie@gmail.com>
2025-10-21 18:14:40 +0800

    added page.tsx under dashboard

---
+++ app/dashboard/vinacabrera/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+export default function Page() {
+    return(
+        <div>
+        <iframe
+        src="https://github.com/vinaaamariee/my_portfolio"
+        title= "My Portfolio GitHub Repository"
+        width="100%"
+        height="800px"
+        style={{border: 'none'}}
+        />
+        </div>
+    )
+}
