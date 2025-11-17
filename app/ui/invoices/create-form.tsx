a3d948295bee43961fa2a5e2c6ca64ba8e11400e
vina marie <cabreravinamarie@gmail.com>
2025-11-17 17:55:55 +0800

    addded create-form.tsx

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,10 +7,11 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '../../lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form  action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -31,6 +32,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 <option key={customer.id} value={customer.id}>
                   {customer.name}
                 </option>
+                
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
