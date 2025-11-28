030c59c8eed984321768f9363228cc7d6e5dc87e
vina marie <cabreravinamarie@gmail.com>
2025-11-28 22:02:13 +0800

    update action.ts

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -2,7 +2,7 @@ import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
  
-export default async function Page() {
+export default async function page() {
   const customers = await fetchCustomers();
  
   return (
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -16,6 +16,7 @@ export default function EditInvoiceForm({
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
+  
 }) { const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
  
   return <form action={updateInvoiceWithId}>{/* ... */}</form>; 
