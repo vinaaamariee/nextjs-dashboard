534a79c5d99db4266f3d99c3474971376ee0db58
vina marie <cabreravinamarie@gmail.com>
2025-11-28 23:21:47 +0800

    updates

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -81,7 +81,11 @@ export default async function InvoicesTable({
               {invoices?.map((invoice) => (
                 <tr
                   key={invoice.id}
-                  className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                  className="w-full border-b py-3 text-sm last-of-type:border-none 
+                  [&:first-child>td:first-child]:rounded-tl-lg 
+                  [&:first-child>td:last-child]:rounded-tr-lg 
+                  [&:last-child>td:first-child]:rounded-bl-lg 
+                  [&:last-child>td:last-child]:rounded-br-lg"
                 >
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
