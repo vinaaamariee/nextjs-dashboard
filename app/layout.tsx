d3424b29314378ff973a436e7642353c8a0b231f
vina marie <cabreravinamarie@gmail.com>
2025-12-02 03:37:18 +0800

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css';
 // Removed `inter` import — not used in layout
 import Link from 'next/link'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
